4a8e41bd269b4ba27960be0c3436bf272b8df48b
Carmen Cabral <71383189+carmencabral@users.noreply.github.com>
2020-11-11 12:53:34 +0000

    Add files via upload

---
+++ 16 Instruçao-Repetiçao-Do-While.cpp
@@ -0,0 +1,21 @@
+// aula16.cpp
+// Instrução DO WHILE
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int cont = 20;
+
+    do
+    {
+        cout << cont << "  ";
+        cont++;
+        
+    } while (cont < 20);
+
+    cout << "\n";
+    return 0;
+}

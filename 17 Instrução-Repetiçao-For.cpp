848852c04157d44b6a5f4eb509dc45d00aecf852
Carmen Cabral <71383189+carmencabral@users.noreply.github.com>
2020-11-11 12:54:37 +0000

    Add files via upload

---
+++ 17 Instrução-Repetiçao-For.cpp
@@ -0,0 +1,19 @@
+// aula17.cpp
+// Instrução de repetição FOR
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for(int x = 0, y = 10; x < 10; x++, y--)
+    {
+        cout << x;
+        cout << "\n";
+        cout << y << " - ";
+
+    }
+
+    return 0;
+}
